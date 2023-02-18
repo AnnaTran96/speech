ca668d9281161e855beb425fbcc1129c82ed44b0
Anna Tran <anna_tran@hotmail.co.uk>
2023-02-18 21:56:03 +0000

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.scss';
+import { Home } from './pages';
 
 const App = () => (
 	<div className='app-container' data-testid='app-container'>
 		<h1 data-testid='app-heading'>Speech</h1>
+		<Home />
 	</div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { About, Error, Results, SavedWords } from '../src/pages/index';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <Error />,
+	},
+	{
+		path: '/about',
+		element: <About />,
+	},
+	{
+		path: '/results',
+		element: <Results />,
+	},
+	{
+		path: '/saved-words',
+		element: <SavedWords />,
+	},
+]);
+
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
-
-reportWebVitals();
