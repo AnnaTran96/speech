3c06669eb8b5688e5589d13a7163580e62fbd37c
Anna Tran <anna_tran@hotmail.co.uk>
2023-02-18 20:43:35 +0000

    refactor templates

--- src/templates/TemplateName.tsx
+++ src/templates/TemplateName.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './TemplateName.module.scss';
 
 const TemplateName = () => (
-	<div className='TemplateName' data-testid='TemplateName'>
+	<div className='templateName container' data-testid='templateName-container'>
 		<h1>TemplateName component</h1>
 	</div>
 );
