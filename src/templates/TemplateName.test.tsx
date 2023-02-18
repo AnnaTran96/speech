944ecb6f3a4567da350522e4cf4b620d9eb5078d
Anna Tran <anna_tran@hotmail.co.uk>
2023-02-18 20:43:35 +0000

    refactor templates

--- src/templates/TemplateName.test.tsx
+++ src/templates/TemplateName.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import TemplateName from './TemplateName';
 
-it('should render ', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<TemplateName />, div);
-	ReactDOM.unmountComponentAtNode(div);
+it('should render TemplateName component', () => {
+	render(<TemplateName />);
+	const container = screen.getByTestId('templateName-container');
+	expect(container).toBeInTheDocument();
 });
