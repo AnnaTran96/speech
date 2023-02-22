b77d269d67f915ffeaafcf43959214a66733ee06
Anna Tran <anna_tran@hotmail.co.uk>
2023-02-22 20:29:29 +0000

    modify routing configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { About, Error, Results, SavedWords } from '../src/pages/index';
 import App from './App';
+import Navbar from './components/Navbar/Navbar';
 import Settings from './pages/Settings/Settings';
 import store from './store';
 
@@ -12,27 +13,42 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 
+const NavbarWrapper = () => {
+	return (
+		<div>
+			<Navbar />
+			<Outlet />
+		</div>
+	);
+};
+
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <App />,
-		errorElement: <Error />,
-	},
-	{
-		path: '/about',
-		element: <About />,
-	},
-	{
-		path: '/results',
-		element: <Results />,
-	},
-	{
-		path: '/saved-words',
-		element: <SavedWords />,
-	},
-	{
-		path: '/settings',
-		element: <Settings />,
+		element: <NavbarWrapper />,
+		children: [
+			{
+				path: '/',
+				element: <App />,
+				errorElement: <Error />,
+			},
+			{
+				path: '/about',
+				element: <About />,
+			},
+			{
+				path: '/results',
+				element: <Results />,
+			},
+			{
+				path: '/saved-words',
+				element: <SavedWords />,
+			},
+			{
+				path: '/settings',
+				element: <Settings />,
+			},
+		],
 	},
 ]);
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as Error } from './Error/Error';
 export { default as Home } from './Home/Home';
 export { default as Results } from './Results/Results';
 export { default as SavedWords } from './SavedWords/SavedWords';
+export { default as Settings } from './Settings/Settings';
