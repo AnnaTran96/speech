679719b566cac6f05fc6d9aa3e36d65ef66be88c
Anna Tran <anna_tran@hotmail.co.uk>
2023-02-23 19:45:13 +0000

    use custom button component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,17 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
 import './Logo.scss';
 
-const Logo = () => (
-	<div className='logo container' data-testid='logo-container'>
-		<div className='logo-shape'>
-			<span className='circle'></span>
-			<div className='sound-curves'>
-				<span className='sound-curve'></span>
-				<span className='sound-curve'></span>
-				<span className='sound-curve'></span>
+const Logo = () => {
+	const navigate = useNavigate();
+
+	const redirectToHome = () => {
+		navigate('/');
+	};
+
+	return (
+		<div className='logo container' data-testid='logo-container'>
+			<div className='logo-shape'>
+				<span className='circle'></span>
+				<div className='sound-curves'>
+					<span className='sound-curve'></span>
+					<span className='sound-curve'></span>
+					<span className='sound-curve'></span>
+				</div>
 			</div>
+			<button className='logo-name' onClick={redirectToHome}>
+				Speech
+			</button>
 		</div>
-		<span className='logo-name'>Speech</span>
-	</div>
-);
+	);
+};
 
 export default Logo;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,44 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../Button/Button';
 import Logo from '../Logo/Logo';
 import SearchBar from '../SearchBar/SearchBar';
 import './Navbar.scss';
 
-const Navbar = () => (
-	<div className='navbar' data-testid='navbar-container'>
-		<div className='logo-searchbar-row'>
-			<div className='navbar-logo'>
-				<Logo />
+const Navbar = () => {
+	const navigate = useNavigate();
+
+	const redirectToAbout = () => navigate('/about');
+	const redirectToSavedWords = () => navigate('/saved-words');
+	const redirectToSettings = () => navigate('/settings');
+
+	return (
+		<div className='navbar' data-testid='navbar-container'>
+			<div className='logo-searchbar-row'>
+				<div className='navbar-logo'>
+					<Logo />
+				</div>
+				<div className='navbar-searchbar'>
+					<SearchBar />
+				</div>
 			</div>
-			<div className='navbar-searchbar'>
-				<SearchBar />
-			</div>
-		</div>
-		<div>
 			<div>
-				<ul className='navbar-links'>
-					<li>
-						<a href='/about'>About</a>
-					</li>
-					<li>
-						<a href='/saved-words'>Saved Words</a>
-					</li>
-					<li>
-						<a href='/settings'>Settings</a>
-					</li>
-				</ul>
+				<div>
+					<div className='navbar-links'>
+						<Button className='link' onClick={redirectToAbout}>
+							About
+						</Button>
+						<Button className='link' onClick={redirectToSavedWords}>
+							Saved Words
+						</Button>
+						<Button className='link' onClick={redirectToSettings}>
+							Settings
+						</Button>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Navbar;
