8fc42c466fb0013b57f94d265f4ba1b22c68ea7a
Anna Tran <anna_tran@hotmail.co.uk>
2023-03-02 20:22:25 +0000

    add event type

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 import styles from './Button.module.scss';
 
 interface ButtonProps {
-	onClick: (e: any) => void;
-	className: string;
-	children?: React.ReactNode;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  className: string;
+  children?: React.ReactNode;
 }
 
 const Button = ({ onClick, className, children }: ButtonProps) => {
-	return (
-		<button onClick={onClick} className={`${styles.button} ${className}`}>
-			{children}
-		</button>
-	);
+  return (
+    <button onClick={onClick} className={`${styles.button} ${className}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
