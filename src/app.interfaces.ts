87bbbc3d76eb3032ffca48c88bff2e038bfd6a82
Anna Tran <anna_tran@hotmail.co.uk>
2023-03-03 23:40:55 +0000

    remove types

--- src/app.interfaces.ts
+++ src/app.interfaces.ts
@@ -1,24 +1,15 @@
-export interface Phonetics {
-  audio: string;
-  text: string;
-}
-
 export interface Definitions {
   definition: string;
-  antonyms: string[];
-  synonyms: string[];
 }
 
 export interface Meanings {
   partOfSpeech: string;
-  antonyms: string[];
   synonyms: string[];
   definitions: Definitions[];
 }
 
 export interface Search {
   word: string;
-  phonetics: Phonetics[];
   meanings: Meanings[];
 }
 
