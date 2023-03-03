72073fe97b82115765bc57ce9c7147d6e9b5785a
Anna Tran <anna_tran@hotmail.co.uk>
2023-03-03 11:51:45 +0000

    remove home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.scss';
-import { Home } from './features/pages';
 
 const App = () => (
-	<div className='app' data-testid='app-container'>
-		<span className='grid-background'></span>
-		<span className='grid-background'></span>
-		<Home />
-	</div>
+  <div className='app container' data-testid='app-container'>
+    <span className='grid-background'></span>
+    <span className='grid-background'></span>
+  </div>
 );
 
 export default App;
