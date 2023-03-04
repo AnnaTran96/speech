1f494a56c69f24574c28c699485206198ab88220
Anna Tran <anna_tran@hotmail.co.uk>
2023-03-04 17:57:11 +0000

    style cursor

--- src/features/pages/Home/Home.tsx
+++ src/features/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 import Button from 'common/components/Button/Button';
+import Card from 'common/components/Card/Card';
 
 import 'features/pages/Home/Home.scss';
 
@@ -14,6 +15,17 @@ const Home = () => {
 
    return (
       <div className='home container' data-testid='home-container'>
+         <Card className='home-cards'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Exercitationem dolorum deserunt expedita totam vitae amet temporibus
+            tenetur corporis blanditiis corrupti quae consequuntur praesentium
+            dicta commodi, neque molestias magnam quo voluptatem. Lorem ipsum
+            dolor, sit amet consectetur adipisicing elit. Exercitationem dolorum
+            deserunt expedita totam vitae amet temporibus tenetur corporis
+            blanditiis corrupti quae consequuntur praesentium dicta commodi,
+            neque molestias magnam quo voluptatem. Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Exercitationem dolorum deserunt
+         </Card>
          <Button className='more-info-btn' onClick={handleGetStarted}>
             More Info
          </Button>
